a4d571102ec8975052b5dbd6615382025758f903
chunchaolee <chunchaolee@gmail.com>
2018-05-22 21:30:50 +0800

    10% off for 3 different books

---
+++ lib/cart.rb
@@ -0,0 +1,29 @@
+class Cart
+  
+  PRICE = 100
+
+  def add(items)
+    @ordered_items = items.map{ |key, value| value }
+  end
+
+  def calculate
+
+    total = 0
+    @ordered_items = @ordered_items.delete_if{ |i| i == 0 }
+
+    while @ordered_items.size > 0
+      if @ordered_items.size == 2
+        total = total + 2 * PRICE * 0.95
+      elsif @ordered_items.size == 3
+        total = total + 3 * PRICE * 0.9
+      else
+        total = total + PRICE
+      end
+      @ordered_items = @ordered_items.map { |i| i -= 1 }
+      @ordered_items = @ordered_items.delete_if{ |i| i == 0 }
+    end
+
+    return total
+  end
+
+end
